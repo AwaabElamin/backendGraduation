ee3d27838524f56fa7940c44a699da0c497c02a2
Awaab Elamin <AwaabElamin@gmail.com>
2022-06-24 01:58:43 -0500

    update user route with neccassry auth

--- routes/users.js
+++ routes/users.js
@@ -3,10 +3,11 @@ const usersController = require('../controllers/users');
 const authorizationController = require('../controllers/auth');
 
 const router = express.Router();
-router.post('',authorizationController.authorize,usersController.create);
+router.post('',usersController.create);
 router.get('',authorizationController.authorize,usersController.getAll);
 router.get('/:id',authorizationController.authorize, usersController.readById);
 router.put('',authorizationController.authorize,usersController.update);
+router.put('/role',authorizationController.authorize,authorizationController.adminAuthorize,usersController.updateRole);
 router.delete('',authorizationController.authorize,usersController.delete);
 
 
